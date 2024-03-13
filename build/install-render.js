27cc75cf791e0ff4291d7befca6e4243aac72933
575792372@qq.com <575792372@qq.com>
2024-03-13 18:23:37 +0800

    add

--- build/install-render.js
+++ build/install-render.js
@@ -1,6 +1,12 @@
 import { defineConfig, loadEnv } from "vite";
 import path from "path";
 
+// 生成打包入口文件 dist
+// 复制components中的render、svg-icon和widgets所有组件到dist/components下
+// 复制styles到dist/styles下
+// 复制hooks到dist/hooks下
+// 复制theme到dist/theme下
+// 生成index入口文件，暴漏install方法
 export default defineConfig(({ command, mode }) => {
   console.log("打包render", path.resolve(__dirname, "../install"));
   return {
